a25abb05853467560ce0ba5d596b5562b0524791
김재원 <dumdum7@hs.ac.kr>
2025-11-26 14:46:53 +0900

    feat: validation 추가

--- src/main/java/com/sprint/sb06deokhugamteam01/dto/book/request/PagingBookRequest.java
+++ src/main/java/com/sprint/sb06deokhugamteam01/dto/book/request/PagingBookRequest.java
@@ -1,5 +1,8 @@
 package com.sprint.sb06deokhugamteam01.dto.book.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 
@@ -8,10 +11,13 @@ import java.time.LocalDateTime;
 @Builder
 public record PagingBookRequest(
      String keyword,
+     @NotBlank(message = "orderBy는 비어 있을 수 없습니다.")
      OrderBy orderBy,
+     @NotBlank(message = "direction는 비어 있을 수 없습니다.")
      SortDirection direction,
      String cursor,
      LocalDateTime after,
+     @PositiveOrZero(message = "limit는 0 이상의 정수여야 합니다.")
      Integer limit
 ) {
 
