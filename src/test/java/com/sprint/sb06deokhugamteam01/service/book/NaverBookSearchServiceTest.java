52b99fd528b5231cc410bea1faaefc61529768f0
김재원 <dumdum7@hs.ac.kr>
2025-12-02 14:40:27 +0900

    test: 테스트 코드 추가

--- src/test/java/com/sprint/sb06deokhugamteam01/service/book/NaverBookSearchServiceTest.java
+++ src/test/java/com/sprint/sb06deokhugamteam01/service/book/NaverBookSearchServiceTest.java
@@ -9,10 +9,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
 @ActiveProfiles("test")
 @DisplayName("NaverBookSearchService 테스트")
 class NaverBookSearchServiceTest {
