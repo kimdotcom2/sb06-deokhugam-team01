d28a57db72adb36a6995f613622197208996155e
김재원 <dumdum7@hs.ac.kr>
2025-12-04 09:09:02 +0900

    test: 테스트 코드 틀 작성

--- src/test/java/com/sprint/sb06deokhugamteam01/service/book/BasicS3StorageServiceTest.java
+++ src/test/java/com/sprint/sb06deokhugamteam01/service/book/BasicS3StorageServiceTest.java
@@ -1,6 +1,7 @@
 package com.sprint.sb06deokhugamteam01.service.book;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -10,6 +11,34 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("BasicS3StorageService 테스트")
 class BasicS3StorageServiceTest {
 
+    @Test
+    @DisplayName("putObject 성공 테스트")
+    void putObject_Success() {
 
+    }
+
+    @Test
+    @DisplayName("deleteObject 성공 테스트")
+    void deleteObject_Success() {
+
+    }
+
+    @Test
+    @DisplayName("deleteObjects 실패 테스트 - 객체 없음")
+    void deleteObjects_Failure_NoSuchS3Object() {
+
+    }
+
+    @Test
+    @DisplayName("getPresignedUrl 성공 테스트")
+    void getPresignedUrl_Success() {
+
+    }
+
+    @Test
+    @DisplayName("getPresignedUrl 실패 테스트 - 객체 없음")
+    void getPresignedUrl_Failure_NoSuchS3Object() {
+        
+    }
 
 }
